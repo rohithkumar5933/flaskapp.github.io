3078ee7b644de6fa7889ecbf1afee88c3fe9f6bd
rohithkumar5933 <70418818+rohithkumar5933@users.noreply.github.com>
2023-03-13 00:55:13 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,10 @@
+# Mobile-Price-Range-Prediction
+Machine Learning Algorithms using Python 3 to predict price ranges of mobile phone
+
+
+Supports both mac and windows users who hava an anaconda navigator installed with jupyterlab and the environments :) 
+
+Used Dataset: https://www.kaggle.com/iabhishekofficial/mobile-price-classification?select=train.csv
+
+![](images/image1.png)
+![](images/image2.png)
---
+++ app.py
@@ -0,0 +1,39 @@
+# Import the libraies
+import numpy as np
+from flask import Flask, request, render_template
+import joblib
+
+# Create the Flask app and load the trained model
+app = Flask(__name__)
+model = joblib.load('models/trained_mobile_svm_updated_final.pkl')
+
+# Define the '/' root route to display the content from index.html
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+# Define the '/predict' route to:
+# - Get form data and convert them to float values
+# - Convert form data to numpy array
+# - Pass form data to model for prediction
+
+@app.route('/predict',methods=['POST'])
+def predict():
+
+    form_data = [x for x in request.form.values()]
+    features = [np.array(form_data)]
+    prediction = model.predict(features)
+
+    text_map={}
+    text_map[0]="should not bemore than 10,000 Rs, less than 10,000 obvious price would be 7000 but might consider upto 10,000"
+    text_map[1]="should not be more than 50,000, usually will come under 30 thousand but, depends between 10,000 - 49,999 based on branding"
+    text_map[2]="should not be more than 95,000 usually will come under 50 thousand, but consider top brands they will charge more"
+    text_map[3]="is abnormal and it might go up from 95 thousand atleast to 2 Lakhs or more"
+    if prediction[0]<0:
+        prediction[0]=0
+    if prediction[0]>3:
+        prediction[0]=3
+    return render_template('index.html', mp =f"{text_map[prediction[0]]}")
+
+if __name__ == '__main__':
+    app.run(debug=False)
