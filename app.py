17bf60a9c766fb54ec59b033e5bc5f8c78d4a8e9
rohithkumar5933 <70418818+rohithkumar5933@users.noreply.github.com>
2023-03-28 19:10:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -28,12 +28,8 @@ def predict():
     text_map[0]="should not bemore than 10,000 Rs, less than 10,000 obvious price would be 7000 but might consider upto 10,000"
     text_map[1]="should not be more than 50,000, usually will come under 30 thousand but, depends between 10,000 - 49,999 based on branding"
     text_map[2]="should not be more than 95,000 usually will come under 50 thousand, but consider top brands they will charge more"
-    text_map[3]="is abnormal and it might go up from 95 thousand atleast to 2 Lakhs or more"
-    if prediction[0]<0:
-        prediction[0]=0
-    if prediction[0]>3:
-        prediction[0]=3
-    return render_template('result.html', prediction =f"{text_map[prediction[0]]}")
+
+    return render_template('result.html', prediction =f"{prediction[0]}")
 
 if __name__ == '__main__':
     app.run(debug=False)
