1bcd8cc9b0a35c6f68dcf4a02671720df6919b26
rohithkumar5933 <70418818+rohithkumar5933@users.noreply.github.com>
2023-03-29 17:23:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -26,7 +26,7 @@ def predict():
 
     text_map={}
     text_map[0]="should not bemore than 10,000 Rs, less than 10,000 obvious price would be 7000 but might consider upto 10,000"
-    text_map[1]="should not be more than 50,000, usually will come under 30 thousand but, depends between 10,000 - 49,999 based on brand
+    text_map[1]="should not be more than 50,000, usually will come under 30 thousand but, depends between 10,000 - 49,999 based on brand"
     return render_template('result.html', prediction =f"{prediction[0]}")
 
 if __name__ == '__main__':
