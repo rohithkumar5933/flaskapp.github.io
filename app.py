f15594189a67415a261b709eb68340384f7c8fba
rohithkumar5933 <70418818+rohithkumar5933@users.noreply.github.com>
2023-05-10 12:03:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -23,11 +23,15 @@ def predict():
     form_data = [x for x in request.form.values()]
     features = [np.array(form_data)]
     prediction = model.predict(features)
-
-    text_map={}
-    text_map[0]="should not bemore than 10,000 Rs, less than 10,000 obvious price would be 7000 but might consider upto 10,000"
-    text_map[1]="should not be more than 50,000, usually will come under 30 thousand but, depends between 10,000 - 49,999 based on brand"
-    return render_template('result.html', prediction =f"{prediction[0]}")
+    data={
+        "0": "10,000 - 20,000",
+        "1": "20,000 - 35,000",
+        "2": "30,00 - 70,000",
+        "3": "50,000 - 1,50,000",
+        "4": "> 1,00,000"
+    }
+	# Format prediction text for display in "index.html"
+    return render_template('result.html', prediction ='Moblile price range should be {}'.format(data.get(str(prediction[0]),"10,00-1,00,000")))
 
 if __name__ == '__main__':
     app.run(debug=False)
